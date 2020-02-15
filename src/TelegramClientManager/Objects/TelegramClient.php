b8f14e42866784c40ecf7a4d1ea3bb97fc5ddcc0
netkas <netkas@intellivoid.info>
2020-02-15 18:40:59 -0300

    Added TelegramChatType to \TelegramClientManager\Abstracts

---
+++ src/TelegramClientManager/Objects/TelegramClient.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace TelegramClientManager\Objects;
+
+
+class TelegramClient
+{
+
+}
---
+++ src/TelegramClientManager/Objects/TelegramClient/Chat.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace TelegramClientManager\Objects\TelegramClient;
+
+
+class Chat
+{
+
+}
