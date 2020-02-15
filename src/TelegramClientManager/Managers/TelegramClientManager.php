e9cfbb898de57fa105366dbf2ce8b512ed8e4e53
netkas <netkas@intellivoid.info>
2020-02-15 20:40:34 -0300

    Added method updateClient() to \TelegramClientManager\Managers > TelegramClientManager

--- src/TelegramClientManager/Managers/TelegramClientManager.php
+++ src/TelegramClientManager/Managers/TelegramClientManager.php
@@ -231,4 +231,48 @@
                 return $ResultsArray;
             }
         }
+
+        /**
+         * Updates an existing Telegram client in the database
+         *
+         * @param TelegramClient $telegramClient
+         * @return bool
+         * @throws DatabaseException
+         */
+        public function updateClient(TelegramClient $telegramClient): bool
+        {
+            $id = (int)$telegramClient->ID;
+            $available = (int)$telegramClient->Available;
+            $account_id = (int)$telegramClient->AccountID;
+            $user = ZiProto::encode($telegramClient->User->toArray());
+            $user = $this->telegramClientManager->database->real_escape_string($user);
+            $chat = ZiProto::encode($telegramClient->Chat->toArray());
+            $chat = $this->telegramClientManager->database->real_escape_string($chat);
+            $session_data = ZiProto::encode($telegramClient->SessionData->toArray());
+            $session_data = $this->telegramClientManager->database->real_escape_string($session_data);
+            $chat_id = $this->telegramClientManager->database->real_escape_string($telegramClient->Chat->ID);
+            $user_id = $this->telegramClientManager->database->real_escape_string($telegramClient->User->ID);
+            $last_activity = (int)time();
+
+            $Query = QueryBuilder::update('telegram_clients', array(
+                'available' => $available,
+                'account_id' => $account_id,
+                'user' => $user,
+                'chat' => $chat,
+                'session_data' => $session_data,
+                'chat_id' => $chat_id,
+                'user_id' => $user_id,
+                'last_activity' => $last_activity
+            ), 'id', $id);
+            $QueryResults = $this->telegramClientManager->database->query($Query);
+
+            if($QueryResults == true)
+            {
+                return true;
+            }
+            else
+            {
+                throw new DatabaseException($Query, $this->telegramClientManager->database->error);
+            }
+        }
     }
